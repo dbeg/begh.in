d5a24e3c4b84b6d3a4f4beeca289a5f844da3e23
Dan Beghin <dbeg@users.noreply.github.com>
2021-10-24 19:52:43 -0500

    feat(style): add a favicon :pizza: (#5)

--- gatsby-config.js
+++ gatsby-config.js
@@ -3,5 +3,18 @@ module.exports = {
     siteUrl: "https://begh.in",
     title: "begh.in",
   },
-  plugins: [],
+  plugins: [
+    {
+      resolve: `gatsby-plugin-manifest`,
+      options: {
+        name: `begh.in`,
+        short_name: `begh.in`,
+        start_url: `/`,
+        background_color: `#eae2b7`,
+        theme_color: `#232129`,
+        display: `standalone`,
+        icon: `./src/images/favicon-512x512.png`,
+      },
+    },
+  ],
 };
